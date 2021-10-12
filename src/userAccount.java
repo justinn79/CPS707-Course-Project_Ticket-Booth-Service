d4a44f87d0d334665038d3e3d39b1b284923485d
Ryan Rodrigues <rcr2255@gmail.com>
2021-10-12 02:34:18 -0400

    Update userAccount.java

--- src/userAccount.java
+++ src/userAccount.java
@@ -6,24 +6,24 @@ import java.io.FileNotFoundException;
 public class userAccount{
 
     //Username, Usertype, available credits
-    private String username;
-    private String userType;
-    private float availableCredit;
+    public String username, userType;
+    public float availableCredit;
 
-    // public userAccount(String username, String userType, float availableCredit){
-    //     this.username = username;
-    //     this.userType = userType;
-    //     this.availableCredit = availableCredit;
-    // }
+    public userAccount(String username, String userType, float availableCredit){
+        this.username = username;
+        this.userType = userType;
+        this.availableCredit = availableCredit;
+    }
 
-    public static void login(String username) throws FileNotFoundException{
+    public static boolean login(String username) throws FileNotFoundException
+    {
         
         boolean found = false;
         String userTemp = "";        
         
         try
         {
-            Scanner scan = new Scanner(new File("currentUsersAccountsFile.txt"));
+            Scanner scan = new Scanner(new File("txtfiles/currentUsersAccountsFile.txt"));
             scan.useDelimiter("\\s+");
 
             while(scan.hasNext() && !found)
@@ -36,16 +36,44 @@ public class userAccount{
                 }
             }
             scan.close();
-            if(found==true){
-                System.out.println("Login Successful");
-            }
-            else{
-                System.out.println("Invalid Login");
-            }
         }
         catch(Exception e)
         {
             System.out.println("Error");
         }
+        return found;
+    }
+
+    public static String getUserCredentials(String username) throws FileNotFoundException
+    {
+        
+        boolean found = false;
+        String userTemp = "";
+        String userType = "";
+        String availableCredit = "";      
+        
+        try
+        {
+            Scanner scan = new Scanner(new File("txtfiles/currentUsersAccountsFile.txt"));
+            scan.useDelimiter("\\s+");
+
+            while(scan.hasNext() && !found)
+            {
+                userTemp = scan.next();
+
+                if (userTemp.trim().equals(username.trim()))
+                {
+                    userType = scan.next();
+                    availableCredit = scan.next();
+                }
+            }
+            scan.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error");
+        }
+        System.out.println(userType + " " + availableCredit);
+        return userType + " " + availableCredit;
     }
 }
