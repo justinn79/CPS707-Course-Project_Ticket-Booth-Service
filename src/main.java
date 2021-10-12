9fdd5fe0f2c12ac9f6c7e8762e2ad7aa4c97d13a
Ryan Rodrigues <rcr2255@gmail.com>
2021-10-12 05:04:35 -0400

    Update main.java

--- src/main.java
+++ src/main.java
@@ -4,22 +4,25 @@ import java.io.FileNotFoundException;
 
 public class main{
 
+    //This method runs the ticket service program
     public static void main(String[] args) throws FileNotFoundException{
 
         boolean done = false;
         boolean loginFlag = false;
+        //empty userAccount object to be fillid after a successful log in
         userAccount user = new userAccount(null, null, 0);
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to Tix Event Ticketing Service\nPlease Login:");
-        String currentUser = "";
         
         while(!(done))
         {
             Scanner commandLine = new Scanner(scanner.nextLine());
             String input = commandLine.next();
             
+            //Checking if user is already logged in
             if (loginFlag == false)
             {
+                //Checking if username is a valid login
                 if (input.equals("login"))
                 {
                     if(!(commandLine.hasNextLine()))
@@ -30,13 +33,14 @@ public class main{
                     if (user.login(username) == true)
                     {
                         System.out.println("Login Successful");
+                        //If username is valid then we fill the userAccount object with the right credentials
                         String userCredentials = user.getUserCredentials(username);
                         String[] credentialsParts = userCredentials.split("-");
                         String userType = credentialsParts[0];
                         double availableCredit = Double.parseDouble(credentialsParts[1]);
                         user.username = username;
                         user.userType = userType;
-                        user.availableCredit = availableCredit;                     
+                        user.availableCredit = availableCredit;
                         loginFlag = true;
                     }
                     else
@@ -44,6 +48,7 @@ public class main{
                         System.out.println("Invalid Login");
                     }
                 }
+                //No transaction is accepted before a login
                 else
                 {
                     System.out.println("You must be logged in before using this application");
@@ -51,11 +56,12 @@ public class main{
             }
             else
             {
-
+                //No double logins
                 if (input.equals("login"))
                 {
                     System.out.println("You are already logged in ");
                 }
+                //Ending the session
                 else if (input.equals("logout"))
                 {
                     done = true;
