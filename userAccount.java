a4d816273ffd5a3f8eee3fd944905a47c8f0eddc
justinn79 <74384294+justinn79@users.noreply.github.com>
2021-10-12 15:31:18 -0400

    Add files via upload

---
+++ userAccount.java
@@ -0,0 +1,177 @@
+import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class userAccount{
+
+    //Username, Usertype, available credits
+    public String username, userType;
+    public double availableCredit;
+
+    //Constructor for the userAccount class
+    public userAccount(String username, String userType, double availableCredit){
+        this.username = username;
+        this.userType = userType;
+        this.availableCredit = availableCredit;
+    }
+
+    //Login method where the inputted username by the user is scanned in the user accounts file to verify their log in.
+    public static boolean login(String username) throws FileNotFoundException
+    {
+        
+        boolean found = false;
+        String userTemp = "";        
+        
+        try
+        {
+            Scanner scan = new Scanner(new File("txtfiles/currentUsersAccountsFile.txt"));
+            scan.useDelimiter("\\s+");
+
+            while(scan.hasNext() && !found)
+            {
+                userTemp = scan.next();
+
+                if (userTemp.trim().equals(username.trim()))
+                {
+                    found = true;
+                }
+            }
+            scan.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error");
+        }
+        return found;
+    }
+
+    //When the user is logged in, this method will use their username to search through the user accounts file to determine their additional info which is their user type and available credits.
+    public static String getUserCredentials(String username) throws FileNotFoundException
+    {
+        
+        boolean found = false;
+        String userTemp = "";
+        String userType = "";
+        String availableCredit = "";      
+        
+        try
+        {
+            Scanner scan = new Scanner(new File("txtfiles/currentUsersAccountsFile.txt"));
+            scan.useDelimiter("\\s+");
+
+            while(scan.hasNext() && !found)
+            {
+                userTemp = scan.next();
+
+                if (userTemp.trim().equals(username.trim()))
+                {
+                    userType = scan.next();
+                    availableCredit = scan.next();
+                }
+            }
+            scan.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error");
+        }
+        System.out.println(userType + " " + availableCredit);
+        return userType + "-" + availableCredit;
+    }
+
+
+    // This method creates a new account for a user and updates the users account file and daily transaction file. This method also checks and catches bad inputs when the user is trying to make an account.
+    public static void createUser(String username, String userType, double availableCredit) throws FileNotFoundException
+    {
+        boolean found = false;
+        String userTemp = "";
+
+        if(username.length() > 15)
+        {
+            System.out.println("Your username must have 15 characters at most.");
+            return;
+        }
+
+        if(!userType.equals("AA") && !userType.equals("FS") && !userType.equals("BS") && !userType.equals("SS"))
+        {
+            System.out.println("The user type does not exist.");
+            return;
+        }
+
+        if(availableCredit > 999999)
+        {
+            System.out.println("The maximum number of credits an account can hold is 999,999");
+            return;
+        }
+        
+        try
+        {
+            Scanner scan = new Scanner(new File("txtfiles/currentUsersAccountsFile.txt"));
+            scan.useDelimiter("\\s+");
+
+            while(scan.hasNext() && !found)
+            {
+                userTemp = scan.next();
+
+                if (userTemp.trim().equals(username.trim()))
+                {
+                    System.out.println("This user already exists.");
+                    found = true;
+                    scan.close();
+                    return;
+                }
+            }
+            scan.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error");
+        }
+
+        // create a user in the usersaccountfile.
+
+        int userLength = username.length();
+        int numDigits = String.valueOf(availableCredit).length();
+
+        int numberOfWhiteSpaces = 15 - userLength;
+        int numberOfZeros = 9 - numDigits;
+
+        String whiteSpace = " ";
+        String zero = "0";
+
+        String createTransactionCode = "01";
+        String userTypeCode = "";
+
+        try(FileWriter myWriter = new FileWriter("txtfiles/currentUsersAccountsFile.txt", true);
+            BufferedWriter bw = new BufferedWriter(myWriter);
+            PrintWriter out = new PrintWriter(bw))
+        {
+            out.print("\n" + username + whiteSpace.repeat(numberOfWhiteSpaces) + " " + userType + " " + zero.repeat(numberOfZeros-1) + availableCredit + "0");
+        }
+        catch(IOException e)
+        {
+            System.out.println("Error");
+        }
+
+        try(FileWriter myWriter = new FileWriter("txtfiles/dailyTransactionFile.txt", true);
+            BufferedWriter bw = new BufferedWriter(myWriter);
+            PrintWriter out = new PrintWriter(bw))
+        {
+            out.print(createTransactionCode + " " + username + whiteSpace.repeat(numberOfWhiteSpaces) + " " + userType + " " + zero.repeat(numberOfZeros-1) + availableCredit + "0" + "\n");
+        }
+        catch(IOException e)
+        {
+            System.out.println("Error");
+        }
+
+    }
+
+    // public static void main(String[] args) throws FileNotFoundException{
+    //     createUser("Josh", "SS", 250.00);
+    // }
+}
