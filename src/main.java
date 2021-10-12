bda6e527be68e55c2ba820ce6ac3c2342d57faa2
justinn79 <74384294+justinn79@users.noreply.github.com>
2021-10-12 15:10:23 -0400

    Add files via upload

--- src/main.java
+++ src/main.java
@@ -1,1 +1,67 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
+public class main{
+
+    public static void main(String[] args) throws FileNotFoundException{
+
+        boolean done = false;
+        boolean loginFlag = false;
+        userAccount user = new userAccount(null, null, 0);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to Tix Event Ticketing Service\nPlease Login:");
+        String currentUser = "";
+        
+        while(!(done))
+        {
+            Scanner commandLine = new Scanner(scanner.nextLine());
+            String input = commandLine.next();
+            
+            if (loginFlag == false)
+            {
+                if (input.equals("login"))
+                {
+                    //System.out.println("Please Enter A Valid Username:");
+                    if(!(commandLine.hasNextLine()))
+					{
+					    continue;
+					}
+                    String username = commandLine.next();
+                    if (user.login(username) == true)
+                    {
+                        System.out.println("Login Successful");
+                        user.username = username;
+                        String userType = user.getUserCredentials(user.username);
+                        loginFlag = true;
+                    }
+                    else
+                    {
+                        System.out.println("Invalid Login");
+                    }
+                }
+                else
+                {
+                    System.out.println("You must be logged in before using this application");
+                }
+            }
+            else
+            {
+
+                if (input.equals("login"))
+                {
+                    System.out.println("You are already logged in ");
+                }
+                else if (input.equals("logout"))
+                {
+                    done = true;
+                    commandLine.close();
+                    scanner.close();
+                    return;
+                } 
+            }
+
+        }
+
+    }
+}
