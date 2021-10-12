af5a747d98ec7a5d838f85b23a13dcb895917c72
justinn79 <74384294+justinn79@users.noreply.github.com>
2021-10-12 04:31:25 -0400

    Add files via upload

--- src/userAccount.java
+++ src/userAccount.java
@@ -1,29 +1,33 @@
 import java.util.Scanner;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 
 public class userAccount{
 
     //Username, Usertype, available credits
-    private String username;
-    private String userType;
-    private float availableCredit;
+    public String username, userType;
+    public double availableCredit;
 
-    // public userAccount(String username, String userType, float availableCredit){
-    //     this.username = username;
-    //     this.userType = userType;
-    //     this.availableCredit = availableCredit;
-    // }
+    public userAccount(String username, String userType, double availableCredit){
+        this.username = username;
+        this.userType = userType;
+        this.availableCredit = availableCredit;
+    }
 
-    public static void login(String username) throws FileNotFoundException{
+    public static boolean login(String username) throws FileNotFoundException
+    {
         
         boolean found = false;
         String userTemp = "";        
         
         try
         {
-            Scanner scan = new Scanner(new File("currentUsersAccountsFile.txt"));
+            Scanner scan = new Scanner(new File("txtfiles/currentUsersAccountsFile.txt"));
             scan.useDelimiter("\\s+");
 
             while(scan.hasNext() && !found)
@@ -36,16 +40,118 @@ public class userAccount{
                 }
             }
             scan.close();
-            if(found==true){
-                System.out.println("Login Successful");
-            }
-            else{
-                System.out.println("Invalid Login");
-            }
         }
         catch(Exception e)
         {
             System.out.println("Error");
         }
+        return found;
     }
+
+    public static String getUserCredentials(String username) throws FileNotFoundException
+    {
+        
+        boolean found = false;
+        String userTemp = "";
+        String userType = "";
+        String availableCredit = "";      
+        
+        try
+        {
+            Scanner scan = new Scanner(new File("txtfiles/currentUsersAccountsFile.txt"));
+            scan.useDelimiter("\\s+");
+
+            while(scan.hasNext() && !found)
+            {
+                userTemp = scan.next();
+
+                if (userTemp.trim().equals(username.trim()))
+                {
+                    userType = scan.next();
+                    availableCredit = scan.next();
+                }
+            }
+            scan.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error");
+        }
+        System.out.println(userType + " " + availableCredit);
+        return userType + " " + availableCredit;
+    }
+
+    public static void createUser(String username, String userType, double availableCredit) throws FileNotFoundException
+    {
+        boolean found = false;
+        String userTemp = "";
+
+        if(username.length() > 15)
+        {
+            System.out.println("Your username must have 15 characters at most.");
+            return;
+        }
+
+        if(!userType.equals("AA") && !userType.equals("FS") && !userType.equals("BS") && !userType.equals("SS"))
+        {
+            System.out.println("The user type does not exist.");
+            return;
+        }
+
+        if(availableCredit > 999999)
+        {
+            System.out.println("The maximum number of credits an account can hold is 999,999");
+            return;
+        }
+        
+        try
+        {
+            Scanner scan = new Scanner(new File("txtfiles/currentUsersAccountsFile.txt"));
+            scan.useDelimiter("\\s+");
+
+            while(scan.hasNext() && !found)
+            {
+                userTemp = scan.next();
+
+                if (userTemp.trim().equals(username.trim()))
+                {
+                    System.out.println("This user already exists.");
+                    found = true;
+                    scan.close();
+                    return;
+                }
+            }
+            scan.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error");
+        }
+
+        // create a user in the usersaccountfile.
+
+        int userLength = username.length();
+        int numDigits = String.valueOf(availableCredit).length();
+
+        int numberOfWhiteSpaces = 15 - userLength;
+        int numberOfZeros = 9 - numDigits;
+
+        String whiteSpace = " ";
+        String zero = "0";
+
+        try(FileWriter myWriter = new FileWriter("txtfiles/currentUsersAccountsFile.txt", true);
+            BufferedWriter bw = new BufferedWriter(myWriter);
+            PrintWriter out = new PrintWriter(bw))
+        {
+            out.print("\n" + username + whiteSpace.repeat(numberOfWhiteSpaces) + " " + userType + " " + zero.repeat(numberOfZeros-1) + availableCredit + "0");
+        }
+        catch(IOException e)
+        {
+            System.out.println("Error");
+        }
+
+    }
+    // public static void main(String[] args) throws FileNotFoundException{
+    //     createUser("Josh", "SS", 250.00);
+    // }
 }
