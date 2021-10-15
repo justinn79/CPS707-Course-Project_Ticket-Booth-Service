666d1648e399113df28d6b7167eb9fea0788555c
justinn79 <74384294+justinn79@users.noreply.github.com>
2021-10-15 13:01:26 -0400

    Add files via upload

--- src/transactions.java
+++ src/transactions.java
@@ -17,25 +17,28 @@ public class transactions {
     // The sell ticket method where it takes the username, event title, ticket price, and the number of tickets. This information is stored in the daily transaction file and will be listed in the available tickets file.
     public static void sellTicket(String sellerUsername, String eventTitle, double ticketPrice, int numberOfTickets) throws FileNotFoundException{
         
-        
+        //If the ticketPrice is greater than 999.99
         if(ticketPrice > 999.99)
         {
             System.out.println("The maximum price for a ticket sale is $999.99");
             return;
         }
 
+        //If the number of characters of eventTitle is greater than 25.
         if(eventTitle.length() > 25)
         {
             System.out.println("The title of the event must have 25 characters at most.");
             return;
         }
 
+        //If the number of tickets is greater than 100.
         if(numberOfTickets > 100)
         {
             System.out.println("The maximum number of tickets you could sell is 100.");
             return;
         }
 
+        //Determining the string lengths of each parameter in this method to properly print the information in the right format to the dailyTransactionfile.txt.
         int userLength = sellerUsername.length();
         int eventLength = eventTitle.length();
         int ticketLength = String.valueOf(numberOfTickets).length();
@@ -52,6 +55,7 @@ public class transactions {
         String sellTransactionCode = "03";
         String userTypeCode = "";
 
+        //Writes this information in the dailyTransactionFile.txt.
         try(FileWriter myWriter = new FileWriter("txtfiles/dailyTransactionFile.txt", true);
             BufferedWriter bw = new BufferedWriter(myWriter);
             PrintWriter out = new PrintWriter(bw))
@@ -64,6 +68,7 @@ public class transactions {
             System.out.println("Error");
         }
 
+        //Writes this information in the availableTicketsFile.txt.
         try(FileWriter myWriter = new FileWriter("txtfiles/availableTicketsFile.txt", true);
             BufferedWriter bw = new BufferedWriter(myWriter);
             PrintWriter out = new PrintWriter(bw))
@@ -89,6 +94,7 @@ public class transactions {
 
         Scanner scanner = new Scanner(System.in);
 
+        //Using a scanner to go through availableTicketsFile.txt and check if the inputted sellers username exists with tickets for sale.
         try
         {
             Scanner scan = new Scanner(new File("txtfiles/availableTicketsFile.txt"));
@@ -106,12 +112,14 @@ public class transactions {
 
                     if(userTemp.equals(sellerUsername))
                     {
+                        //If the buyer wants to buy more tickets than the number of tickets available
                         if(Integer.parseInt(totalTicketsTemp) < numberOfTickets)
                         {
                             System.out.println("There are an insufficient number of tickets for sale.");
                             found = true;
                             break;
                         }
+                        // The purchase information will print to the console if the sellers username exists.
                         System.out.println("Cost per ticket: " + ticketCostTemp);
                         System.out.println("Number of tickets your purchasing: " + numberOfTickets);
                         double totalCost = Double.parseDouble(ticketCostTemp) * numberOfTickets;
@@ -121,8 +129,10 @@ public class transactions {
                         Scanner commandLine = new Scanner(scanner.nextLine());
                         String input = commandLine.next();
 
+                        //If the user inputs "Y"
                         if(input.equals("Y"))
                         {
+                            //If the users available number of credits is less than the total cost of the tickets they want to purchase.
                             if(availableCredit < totalCost)
                             {
                                 System.out.println("You do not have enough credits to make this purchase. You have: $" + availableCredit);
@@ -133,6 +143,7 @@ public class transactions {
                             System.out.println("Purchase successful");
                         }
 
+                        //If the user inputs "N"
                         if(input.equals("N"))
                         {
                             System.out.println("Purchase cancelled.");
@@ -156,6 +167,7 @@ public class transactions {
 
         // //Updating the buy information on the daily transaction file.
 
+        //Determining the string lengths of user information variables to properly print the information in the right format to the dailyTransactionfile.txt.
         int userLength = sellerUsername.length();
         int eventLength = eventTitle.length();
         int ticketLength = String.valueOf(numberOfTickets).length();
@@ -171,6 +183,7 @@ public class transactions {
 
         String buyTransactionCode = "04";
 
+        //Writes to the dailyTransactionFile.txt.
         try(FileWriter myWriter = new FileWriter("txtfiles/dailyTransactionFile.txt", true);
             BufferedWriter bw = new BufferedWriter(myWriter);
             PrintWriter out = new PrintWriter(bw))
@@ -182,7 +195,7 @@ public class transactions {
             System.out.println("Error");
         }
 
-        // Deducting the amount sold from the seller's inventory
+        // Deducting the amount sold from the seller's inventory by identifying the seller in the text file using a scanner.
         try
         {
             Scanner scan = new Scanner(new File("txtfiles/availableTicketsFile.txt"));
@@ -207,10 +220,12 @@ public class transactions {
 
                     if(userTemp.equals(sellerUsername))
                     {
+                        //Determining the number of zeros needed to fit the format on the availableTicketsFile.txt.
                         int numberOfZerosTicketAmount2 = totalTicketsTemp.length() - ticketLength;
                         totalTicketsTemp = ticketScan.next();
                         String oldLine = (eventTitle + whiteSpace.repeat(numberOfWhiteSpacesEvent) + " " +  sellerUsername + whiteSpace.repeat(numberOfWhiteSpacesUser) + " " + totalTicketsTemp);
                         String newLine = (eventTitle + whiteSpace.repeat(numberOfWhiteSpacesEvent) + " " +  sellerUsername + whiteSpace.repeat(numberOfWhiteSpacesUser) + " " + zero.repeat(numberOfZerosTicketAmount2-1) + (Integer.parseInt(totalTicketsTemp) - numberOfTickets));
+                        //Replacing the old line in the file with the new line.
                         fileText = fileText.replaceAll(oldLine, newLine);
                         FileWriter writer = new FileWriter("txtfiles/availableTicketsFile.txt");
                         writer.append(fileText);
@@ -228,6 +243,7 @@ public class transactions {
 
         // Deducting the amount of user credits after a purchase has been made.
 
+        //Determining the string lengths of user information variables to properly print the information in the right format to the currentUsersAccount.txt.
         int usernameLength = username.length();
         int numDigits = String.valueOf(availableCredit).length();
 
@@ -243,6 +259,7 @@ public class transactions {
         String whiteSpaces = " ";
         String zeros = "0";
         
+        //Scanner to go through the currentUsersAccountFile.txt and update the buyers available credit value after a purchase.
         try
         {
             Scanner scan1 = new Scanner(new File("txtfiles/currentUsersAccountsFile.txt"));
@@ -267,6 +284,7 @@ public class transactions {
                 {
                     String oldLine = (username + whiteSpaces.repeat(numberOfWhiteSpaces) + " " + userType + " " + zeros.repeat(numberOfZeros-1) + availableCredit + "0");
                     String newLine = (username + whiteSpaces.repeat(numberOfWhiteSpaces) + " " + userType + " " + zeros.repeat(numberOfZerosForNewValue-1) + newCreditValue + "0");
+                    //Replacing the old line in the file with the new line.
                     fileText = fileText.replaceAll(oldLine, newLine);
                     FileWriter writer = new FileWriter("txtfiles/currentUsersAccountsFile.txt");
                     writer.append(fileText);
@@ -308,6 +326,7 @@ public class transactions {
 
                 if (userTemp.trim().equals(sellerUsername.trim()))
                 {
+                    //Determining the string lengths of user information variables to properly print the information in the right format to the currentUsersAccount.txt.
                     double newSellersCreditValue = userAvailableCredit + totalCost;
                     int newSellersCreditValueDigits = String.valueOf(newSellersCreditValue).length();
                     int numberOfZerosForSellersValue = 9 - newSellersCreditValueDigits;
@@ -317,6 +336,7 @@ public class transactions {
 
                     String oldLine = (sellerUsername + whiteSpaces.repeat(numberOfWhiteSpacesSeller) + " " + userType + " " + zeros.repeat(numberOfZerosOldSeller-1) + userAvailableCredit + "0");
                     String newLine = (sellerUsername + whiteSpaces.repeat(numberOfWhiteSpacesSeller) + " " + userType + " " + zeros.repeat(numberOfZerosForSellersValue-1) + newSellersCreditValue + "0");
+                    //Replacing the old line in the file with the new line.
                     fileText = fileText.replaceAll(oldLine, newLine);
                     FileWriter writer = new FileWriter("txtfiles/currentUsersAccountsFile.txt");
                     writer.append(fileText);
@@ -337,6 +357,7 @@ public class transactions {
     {
         // Adding the specified amount of user credits to the buyer from the seller for the refund.
 
+        //Determining the string lengths of user information variables to properly print the information in the right format to the currentUsersAccount.txt.
         int usernameLength = username.length();
         int sellerUsernameLength = sellerUsername.length();
 
@@ -350,6 +371,7 @@ public class transactions {
         String userType = "";
         double userAvailableCredit;
         
+        //Using a scanner to go through the currentUsersAccountsFile.txt file and update the buyers available credit value.
         try
         {
             Scanner scan1 = new Scanner(new File("txtfiles/currentUsersAccountsFile.txt"));
@@ -372,6 +394,7 @@ public class transactions {
 
                 if (userTemp.trim().equals(username.trim()))
                 {
+                    //Determining the string lengths of user information variables to properly print the information in the right format to the currentUsersAccount.txt.
                     int numDigits = String.valueOf(userAvailableCredit).length();
                     int numOfZeros = 9 - numDigits;
 
@@ -384,6 +407,7 @@ public class transactions {
                     // System.out.println(oldLine);
                     String newLine = (username + whiteSpaces.repeat(numberOfWhiteSpaces) + " " + userType + " " + zeros.repeat(numOfZerosForNewAmount-1) + addedAmount + "0");
                     // System.out.println(newLine);
+                    //Replacing the old line in the file with the new line.
                     fileText = fileText.replaceAll(oldLine, newLine);
                     FileWriter writer = new FileWriter("txtfiles/currentUsersAccountsFile.txt");
                     writer.append(fileText);
@@ -401,6 +425,7 @@ public class transactions {
 
         // Deducting the specified amount of user credits from the seller for the refund.
         
+        //Using a scanner to go through the currentUsersAccountsFile.txt file and update the sellers available credit value.
         try
         {
             Scanner scan1 = new Scanner(new File("txtfiles/currentUsersAccountsFile.txt"));
@@ -423,6 +448,7 @@ public class transactions {
 
                 if (userTemp.trim().equals(sellerUsername.trim()))
                 {
+                    //Determining the string lengths of user information variables to properly print the information in the right format to the currentUsersAccount.txt.
                     int numDigits = String.valueOf(userAvailableCredit).length();
                     int numOfZeros = 9 - numDigits;
 
@@ -435,6 +461,7 @@ public class transactions {
                     // System.out.println(oldLine);
                     String newLine = (sellerUsername + whiteSpaces.repeat(numberOfWhiteSpacesSeller) + " " + userType + " " + zeros.repeat(numOfZerosForNewAmount-1) + subtractedAmount + "0");
                     // System.out.println(newLine);
+                    //Replacing the old line in the file with the new line.
                     fileText = fileText.replaceAll(oldLine, newLine);
                     FileWriter writer = new FileWriter("txtfiles/currentUsersAccountsFile.txt");
                     writer.append(fileText);
@@ -453,9 +480,11 @@ public class transactions {
         // Saving this information to the daily transaction file.
 
         String refundTransactionCode = "05";
+        //Determining the number of zeros for the credit amount to match the format within the dailyTransactionFile.txt.
         int numOfDigitsCreditAmount = String.valueOf(creditAmount).length();
         int numOfZerosForCreditAmount = 9 - numOfDigitsCreditAmount;
 
+        //Writes to the dailyTransactionFile.txt.
         try(FileWriter myWriter = new FileWriter("txtfiles/dailyTransactionFile.txt", true);
             BufferedWriter bw = new BufferedWriter(myWriter);
             PrintWriter out = new PrintWriter(bw))
@@ -471,8 +500,8 @@ public class transactions {
     //This addCredit method allows a user to add credits into their account and also allows an admin to add credits to an existing account of their choice.
     public static void addCredit(double creditAmount, String username)
     {
+        //Determining the string lengths of user information variables to properly print the information in the right format to the currentUsersAccount.txt.
         int usernameLength = username.length();
-
         int numberOfWhiteSpaces = 15 - usernameLength;
 
         String whiteSpaces = " ";
@@ -482,6 +511,7 @@ public class transactions {
         String userType = "";
         double userAvailableCredit;
 
+        //Scanner to go through the currentUsersAccountsFile.txt and locates the username inputted to update their credit value.
         try
         {
             Scanner scan1 = new Scanner(new File("txtfiles/currentUsersAccountsFile.txt"));
@@ -504,6 +534,7 @@ public class transactions {
 
                 if (userTemp.trim().equals(username.trim()))
                 {
+                    //Determining the string lengths of user information variables to properly print the information in the right format to the currentUsersAccount.txt.
                     int numDigits = String.valueOf(userAvailableCredit).length();
                     int numOfZeros = 9 - numDigits;
 
@@ -516,6 +547,7 @@ public class transactions {
                     // System.out.println(oldLine);
                     String newLine = (username + whiteSpaces.repeat(numberOfWhiteSpaces) + " " + userType + " " + zeros.repeat(numOfZerosForNewAmount-1) + addedAmount + "0");
                     // System.out.println(newLine);
+                    //Replaces the old line in the text file with the new line.
                     fileText = fileText.replaceAll(oldLine, newLine);
                     FileWriter writer = new FileWriter("txtfiles/currentUsersAccountsFile.txt");
                     writer.append(fileText);
@@ -535,6 +567,7 @@ public class transactions {
 
         boolean found1 = false;
 
+        //Scanner to go through the currentUserAccountsFile.txt and print the information of the addCredit method to the dailyTransactionFile.txt.
         try
         {
             Scanner scan1 = new Scanner(new File("txtfiles/currentUsersAccountsFile.txt"));
@@ -557,9 +590,11 @@ public class transactions {
                 if (userTemp.trim().equals(username.trim()))
                 {
                     String addCreditTransactionCode = "06";
+                    //Determining the string lengths of user information variables to properly print the information in the right format to the currentUsersAccount.txt.
                     int numOfDigitsAvailableCredit = String.valueOf(userAvailableCredit).length();
                     int numOfZerosForAvailableCredit= 9 - numOfDigitsAvailableCredit;
 
+                    //Writes to the dailyTransactionFile.txt.
                     try(FileWriter myWriter = new FileWriter("txtfiles/dailyTransactionFile.txt", true);
                         BufferedWriter bw = new BufferedWriter(myWriter);
                         PrintWriter out = new PrintWriter(bw))
