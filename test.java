1b32f2cc60da2d02fd9121c2a061daf95c8e324f
Ryan Rodrigues <rcr2255@gmail.com>
2021-10-27 16:39:48 -0400

    Delete test.java

--- test.java
+++
@@ -1,7 +0,0 @@
-public class test {
-    public static void main(String[] args){
-        userAccount user = new userAccount(null, null, 0);
-
-        user.createUser("Reanne", "FS", 500.00);
-    }
-}
